b2b16109eddacbe03c07a9b5ad35fc04bdee838c
Chad Wilcomb <chadwilcomb@gmail.com>
2015-10-22 10:09:19 -0400

    added some js and sass files

---
+++ js/color-arrays.js
@@ -0,0 +1,11 @@
+//divergent red-blue choropleth polygon (DMA) map
+var cip_explorer_map = ['rgb(178,24,43)','rgb(239,138,98)','rgb(253,219,199)','rgb(247,247,247)','rgb(209,229,240)','rgb(103,169,207)','rgb(33,102,172)'];
+
+//qualitative pastel-ish color scheme for dot plot
+var cip_explorer_dotplot = ['#86ba14', '#1fbdcc', '#fbb64d', '#ec4561', '#3e3970', '#c4e876', '#6ee2e1', '#fecd9f', '#fd8698', '#a096db'];
+
+// 20-color unique scheme based off of Chrome's Material color scheme with an alternating pattern (one repeat)
+var network_viewer = ['#F44336','#9C27B0','#3F51B5','#03A9F4','#009688','#8BC34A','#FFEB3B','#FF9800','#795548','#607D8B','#E91E63','#673AB7','#2196F3','#00BCD4','#4CAF50','#CDDC39','#FFC107','#FF5722','#9E9E9E','#F44336'];
+
+// 19-color material design scheme from Chrome Dev Tools color-picker
+var google_material = ['#F44336','#E91E63','#9C27B0','#673AB7','#3F51B5','#2196F3','#03A9F4','#00BCD4','#009688','#4CAF50','#8BC34A','#CDDC39','#FFEB3B','#FFC107','#FF9800','#FF5722','#795548','#9E9E9E','#607D8B'];
